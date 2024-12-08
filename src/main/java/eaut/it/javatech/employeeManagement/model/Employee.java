961b3416aeebc5f05a817608c961e451da2cf542
luanp-eaut <luanp@eaut.edu.vn>
2024-12-08 14:52:01 +0700

    save 2nd step

---
+++ src/main/java/eaut/it/javatech/employeeManagement/model/Employee.java
@@ -0,0 +1,18 @@
+package eaut.it.javatech.employeeManagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+	private Long id;
+	private String fullName;
+	private String title;
+	private String email;
+	private String telephone;
+}
