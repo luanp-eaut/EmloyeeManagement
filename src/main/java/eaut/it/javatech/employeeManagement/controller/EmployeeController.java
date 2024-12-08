f794fada964d7de874b7798a414941304a1ad5cc
luanp-eaut <luanp@eaut.edu.vn>
2024-12-08 15:26:12 +0700

    save step2

--- src/main/java/eaut/it/javatech/employeeManagement/controller/EmployeeController.java
+++ src/main/java/eaut/it/javatech/employeeManagement/controller/EmployeeController.java
@@ -2,7 +2,13 @@ package eaut.it.javatech.employeeManagement.controller;
 
 import org.springframework.stereotype.Controller;
 
+import eaut.it.javatech.employeeManagement.service.EmployeeService;
+
 @Controller
 public class EmployeeController {
+	private EmployeeService employeeService;
 
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
 }
--- src/main/java/eaut/it/javatech/employeeManagement/service/EmployeeService.java
+++ src/main/java/eaut/it/javatech/employeeManagement/service/EmployeeService.java
@@ -2,6 +2,15 @@ package eaut.it.javatech.employeeManagement.service;
 
 import org.springframework.stereotype.Service;
 
+import eaut.it.javatech.employeeManagement.repository.EmployeeRepository;
+
 @Service
 public class EmployeeService {
+	private EmployeeRepository employeeRepository;
+
+	public EmployeeService(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+	
+	
 }
